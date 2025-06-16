c5d8811cea4ab65fcb26d624c473f03bfab89d8d
Chandrashekhar Tripathi <tripathics@outlook.com>
2025-06-17 02:06:55 +0530

    2 x esc to hide buf

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Mapping from <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<Esc><Esc>', '<cmd>hide<CR>', { desc = 'Hide current buffer' })
 
 -- Use CTRL+<hjkl> to switch between windows
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
