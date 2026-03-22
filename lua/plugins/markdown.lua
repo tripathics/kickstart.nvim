612e9a122a58cd31fbff9cbd0b55cb26f499f95e
Chandrashekhar Tripathi <tripathics@outlook.com>
2026-03-23 01:14:28 +0530

    Learnt keybinds, so add delay to which key

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,9 @@
+---@type LazySpec[]
 return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
     build = 'cd app && npm install',
-  },
-  { -- render markdown inside neovim
-    'OXY2DEV/markview.nvim',
-    lazy = false,
   },
 }
